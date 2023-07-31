e8ccee5873ee2a7bed9ea81430e64a865095c359
Luis Tinta <79492525+lethc@users.noreply.github.com>
2023-07-31 12:26:48 -0500

    update

--- archlinux.sh
+++ archlinux.sh
@@ -4,9 +4,12 @@
 hostname="archlinux"
 drive="/dev/nvme0n1"  # Change this to your drive, e.g., /dev/nvme0n1
 grub="1GiB"
-swap_size="4GiB"    # Swap partition size
-root_size="50GiB"   # Root partition size
+swap_size="5GiB"    # Swap partition size
+root_size="55GiB"   # Root partition size
 home_size="100%"  # Home partition size (remaining space)
+
+#Update System clock
+timedatectl set-timezone America/Lima
 
 # Ensure we have the latest package list
 pacman -Sy
@@ -20,15 +23,18 @@ parted $drive mkpart primary ext4 $root_size $home_size # For Home
 parted $drive set 1 esp on
 
 # Format the partitions
-mkswap ${drive}1
-mkfs.ext4 ${drive}2
+mkfs.fat -F32 ${drive}1
+mkswap ${drive}2
 mkfs.ext4 ${drive}3
+mkfs.ext4 ${drive}4
 
 # Mount the partitions
-mount ${drive}2 /mnt
+mkdir -p /mnt/boot/efi # For GRUB UEFI
+mount ${drive}1 /mnt/boot/efi/
+swapon ${drive}2
+mount ${drive}3 /mnt # For Root
 mkdir /mnt/home
-mount ${drive}3 /mnt/home
-swapon ${drive}1
+mount ${drive}4 /mnt/home # For Home
 
 # Install Arch Linux base system
 pacstrap /mnt base base-devel linux-lts linux-lts-headers linux-firmware amd-ucode nano vim git sudo networkmanager dhcpcd pulseaudio bluez wpa_supplicant 
@@ -60,7 +66,7 @@ arch-chroot /mnt passwd
 
 # Install the bootloader (assuming you're using GRUB)
 arch-chroot /mnt pacman -S grub efibootmgr dosfstools mtools
-arch-chroot /mnt grub-install --target=x86_64-efi --bootloader-id=arch_grub --recheck
+arch-chroot /mnt grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
 # Install additional packages (you can customize this according to your needs)
