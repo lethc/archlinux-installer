df1188e8d1fc3348240371a75c28f15762279be1
lethc <79492525+lethc@users.noreply.github.com>
2023-08-02 19:23:48 -0500

    Update archlinux.sh

--- archlinux.sh
+++ archlinux.sh
@@ -117,7 +117,7 @@ echo "###############################"
 echo "#####   Enable Services   #####"
 echo "###############################"
 
-arch-chroot /mnt systemctl enable dhcpcd.service NetworkManager.service sddm.service
+arch-chroot /mnt systemctl enable dhcpcd.service NetworkManager.service sddm.service cronie.service
 
 # Finish and unmount
 echo "###############################"
