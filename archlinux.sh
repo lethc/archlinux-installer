d8671105daff0a281a479138cc4219c242cf6b12
lethc <79492525+lethc@users.noreply.github.com>
2024-06-20 11:05:34 -0500

    Update archlinux.sh

--- archlinux.sh
+++ archlinux.sh
@@ -130,7 +130,7 @@ echo "##        Installing GRUB bootloader            ##"
 echo "##################################################"
 
 
-arch-chroot /mnt pacman -S grub efibootmgr dosfstools mtools os-prober
+arch-chroot /mnt pacman -S grub efibootmgr dosfstools mtools os-prober ntfs-3g
 arch-chroot /mnt grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
